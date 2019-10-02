9adc45127db352c0f428f474dd085a17b07a3744
freysn <steffen.frey@visus.uni-stuttgart.de>
2019-10-02 18:07:58 +0200

    x

--- helper_assert.h
+++ helper_assert.h
@@ -21,6 +21,15 @@ namespace helper
       fprintf(stderr, "Assertion '%s' failed, file '%s' line '%d', %s.", expression, file, line, ss.str().c_str());
       abort();
     }
+
+    template<typename M0, typename M1, typename M2>
+    inline void _assert(const char* expression, const char* file, int line, M0 message0, M1 message1, M2 message2)
+    {
+      std::stringstream ss;
+      ss << "<|" << message0 << "|" << message1<< "|" << message2<< "|>";
+      fprintf(stderr, "Assertion '%s' failed, file '%s' line '%d', %s.", expression, file, line, ss.str().c_str());
+      abort();
+    }
     
     inline void _assert(const char* expression, const char* file, int line)
     {
@@ -35,6 +44,7 @@ namespace helper
 #define hassert(EXPRESSION) ((EXPRESSION) ? (void)0 : helper::_assert(#EXPRESSION, __FILE__, __LINE__))
 #define hassertm(EXPRESSION,MESSAGE) ((EXPRESSION) ? (void)0 : helper::_assert(#EXPRESSION, __FILE__, __LINE__, MESSAGE))
 #define hassertm2(EXPRESSION,MESSAGE0, MESSAGE1) ((EXPRESSION) ? (void)0 : helper::_assert(#EXPRESSION, __FILE__, __LINE__, MESSAGE0, MESSAGE1))
+#define hassertm3(EXPRESSION,MESSAGE0, MESSAGE1, MESSAGE2) ((EXPRESSION) ? (void)0 : helper::_assert(#EXPRESSION, __FILE__, __LINE__, MESSAGE0, MESSAGE1, MESSAGE2))
 #endif
 }
     
